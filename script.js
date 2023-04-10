374067b2b3005837f0ce273c7e164d6b102eed36
SHRISHTY MALVIYA <103518988+BilliBup@users.noreply.github.com>
2023-04-10 19:22:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function fibonacci(num) {
 // your code here
- return fibonacci1(num);
-}
-funtion fibonacci1(n){
-		 if (n === 0) {
+  if (num === 0) {
     return 0;
-  } else if (n === 1) {
+  } else if (num === 1) {
     return 1;
   } else {
-    return fibonacci1(n - 1) + fibonacci1(n - 2);
+    return fibonacci1(num - 1) + fibonacci1(num - 2);
   }
 }
 
